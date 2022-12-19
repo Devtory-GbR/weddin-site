733796ca30ef7fe21ee0174696ab4d440b2f2adf
ThunderAnimal <martinweber.9393@gmail.com>
2022-12-19 22:30:22 +0100

    adds imprint page

---
+++ pages/imprint.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <div class="app-container">
+      <h3 class="title is-3">Impressum</h3>
+      <p>
+        <b>Martin Weber</b> <br />
+        Wesenberger Ring 2 <br />
+        12359 Berlin
+      </p>
+      <br />
+      <h5 class="title is-5">Kontakt</h5>
+      <p>
+        E-Mail:
+        <a href="mailto:martinweber.9393@gmail.com"
+          >martinweber.9393@gmail.com</a
+        >
+      </p>
+      <br />
+      <h5 class="title is-5">Vertrieb und Verkauf vom Premium Paket</h5>
+      <p>
+        Der Vertrieb und Verkauf von dem Premium-Paket wird über die
+        <a href="https://www.devtory.io/impressum" target="_blank"
+          >Devtory GbR</a
+        >
+        abgewickelt. <br />
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ImprintPage',
+  transition: 'slide-bottom',
+};
+</script>
+
+<style lang="scss">
+@import './../assets/css/variables';
+</style>
+
+
