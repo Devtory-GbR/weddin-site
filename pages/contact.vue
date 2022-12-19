f66f278e4fb37946d0b80c028a24f9d620b0fe69
ThunderAnimal <martinweber.9393@gmail.com>
2022-12-19 22:34:10 +0100

    adds contact page

---
+++ pages/contact.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <div class="app-container">
+      <h3 class="title is-3">Kontakt</h3>
+      <p>
+        Du hast Fragen, Anmerkungen oder Wünsche? <br />
+        Dann schreibe mich einfach an. 😄
+      </p>
+      <br />
+      <p>
+        E-Mail:
+        <a href="mailto:martinweber.9393@gmail.com"
+          >martinweber.9393@gmail.com</a
+        >
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ContactPage',
+  transition: 'slide-bottom',
+};
+</script>
+
+<style lang="scss">
+@import './../assets/css/variables';
+</style>
+
+
