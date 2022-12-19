90968c34e0777302a2208d1fcd08730ef841fb71
ThunderAnimal <martinweber.9393@gmail.com>
2022-12-19 23:16:16 +0100

    adds privacy page

---
+++ pages/privacy.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <div class="app-container">
+      <h3 class="title is-3">Datenschutzerklärung</h3>
+      <nuxt-content :document="privacyContent" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'PrivacyPage',
+  transition: 'slide-bottom',
+  async asyncData({ $content }) {
+    const privacyContent = await $content('privacy').fetch();
+    return {
+      privacyContent,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+@import './../assets/css/variables';
+</style>
+
+
